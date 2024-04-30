ae6ca249b7736695760de4e2aa049acc4bb359d1
Cristhian Carrillo <cristhiancarrillo99@hotmail.com>
2024-04-30 14:30:39 -0500

    formulario

--- resources/views/pages/sistema/eventos.blade.php
+++ resources/views/pages/sistema/eventos.blade.php
@@ -37,7 +37,13 @@
             </table>
         </div>
         <div>
-
+            <form class="form" action="" method="post">
+                <svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24"><path fill="white" d="M21 17V8H7v9zm0-14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h1V1h2v2h8V1h2v2zm-3.47 8.06l-4.44 4.44l-2.68-2.68l1.06-1.06l1.62 1.62L16.47 10zM3 21h14v2H3a2 2 0 0 1-2-2V9h2z"/></svg>
+                <input type="text" class="input" placeholder="Nombre de Evento">
+                <input type="date" class="input">
+                <input type="text" class="input" placeholder="Hora">
+                <button class="submit">Submit</button>
+            </form>
         </div>
     </div>
 @endsection
