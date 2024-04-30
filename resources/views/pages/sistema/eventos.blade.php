5dd54058e53c55b17dcc14e2b8fda0dd67d6db45
Cristhian Carrillo <cristhiancarrillo99@hotmail.com>
2024-04-30 12:41:59 -0500

    Creacion de tabla de Eventos

--- resources/views/pages/sistema/eventos.blade.php
+++ resources/views/pages/sistema/eventos.blade.php
@@ -4,5 +4,36 @@
 
 @section('title-page', 'Eventos')
 
+@section('style')
+<link rel="stylesheet" href="{{ asset('css/eventos.css')}}">
+    
+@endsection
+
 @section('content')
+    <div id="contenido">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Ingresado</th>
+                        <th>Editado</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nombre1</td>
+                        <td>Fecha1</td>
+                        <td>Hora1</td>
+                        <td>Ingresado1</td>
+                        <td>Editado1</td>
+                        <td>Estado1</td>
+                    </tr>
+                    <!-- Puedes agregar más filas según sea necesario -->
+                </tbody>
+            </table>
+        <div>d</div>
+    </div>
 @endsection
