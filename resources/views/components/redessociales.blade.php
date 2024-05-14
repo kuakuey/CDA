2abec0912a9497d64a0fe72fe13e6954a4d9d457
Cristhian Carrillo <cristhiancarrillo99@hotmail.com>
2024-05-14 12:49:09 -0500

    Cambios en login

--- resources/views/components/redessociales.blade.php
+++ resources/views/components/redessociales.blade.php
@@ -13,7 +13,7 @@
 
 
     <?php
-    $numeroTelefono = "+593995737927"; 
+    $numeroTelefono = "+593995737926"; 
     $mensajeEnviar = "Hola Casa de Avivamiento!";
 
     $enlaceWhatsApp = "https://wa.me/$numeroTelefono?text=$mensajeEnviar";
